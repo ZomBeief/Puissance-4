7302d4a0378e4eec5c8ee17ed31061e0ab1fd5de
Adriniqueswann <60875644+Adriniqueswann@users.noreply.github.com>
2020-02-10 11:27:05 +0100

    menu fonction

---
+++ menu.c
@@ -0,0 +1,22 @@
+void menu(){
+
+		printf("1. I.A VS I.A \n");
+		printf("2. Joueur VS I.A");
+		printf("3. Joueur VS Joueur");
+
+	switch (menu){
+		case 1 :
+			printf("I.A VS I.A");
+			//I.A VS I.A
+			break;
+		case 2 :
+			printf("Joueur VS I.A");
+			//Joueur VS Joueur
+			break;
+		case 3 :
+			printf("Joueur VS Jouer");
+			//Joueur vs Joueur
+		break;
+}
+
+	
